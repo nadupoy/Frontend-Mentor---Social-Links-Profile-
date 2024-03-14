9caeb79f9d35c8528717c24c3981492a49e292c4
nadupoy <gnsampao@gmail.com>
2024-03-14 11:09:04 +0300

    Added hyperlinks to anchor tags.

--- src/index.js
+++ src/index.js
@@ -16,19 +16,19 @@ const Header = () => {
 const SocialLinks = () => {
   return (
     <nav>
-      <a>
+      <a href="https://github.com/" target="_blank">
         <button>GitHub</button>
       </a>
-      <a>
+      <a href="https://www.frontendmentor.io/" target="_blank">
         <button>Frontend Mentor</button>
       </a>
-      <a>
+      <a href="https://ke.linkedin.com/" target="_blank">
         <button>LinkedIn</button>
       </a>
-      <a>
+      <a href="https://twitter.com/?lang=en" target="_blank">
         <button>Twitter</button>
       </a>
-      <a>
+      <a href="https://www.instagram.com/" target="_blank">
         <button>Instagram</button>
       </a>
     </nav>
@@ -49,7 +49,20 @@ const Footer = () => {
   return (
     <>
       <p>
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="https://github.com/nadupoy" target="_blank">Nadupoy</a>.
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://github.com/nadupoy" target="_blank">
+          Nadupoy
+        </a>
+        .
+      </p>
+
+      <p>
+        <a href="https://icons8.com/icon/1406/hand-cursor" target="_blank">Hand Cursor</a> icon by <a href="https://icons8.com/" target="_blank">
+          Icons8</a>.
       </p>
     </>
   );
@@ -59,4 +72,4 @@ const main = ReactDOM.createRoot(document.getElementById("main"));
 main.render(<Profile />);
 
 const footer = ReactDOM.createRoot(document.getElementById("footer"));
-footer.render(<Footer/>);
+footer.render(<Footer />);
